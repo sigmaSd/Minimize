3b20ee3bbc8b1e77521c5b581f09a34c6c821077
Nbiba Bedis <bedisnbiba@gmail.com>
2022-08-13 09:57:33 +0100

    change cwd to targets, handle ctrlc

--- scan.py
+++ scan.py
@@ -2,23 +2,33 @@
 
 from ptyprocess import PtyProcessUnicode
 import sys
-p = PtyProcessUnicode.spawn(["deno", "run", "--unstable"] + sys.argv[1:])
+import os
+
+file = sys.argv[1]
+# change cwd to file cwd
+cwd = file.rsplit('/', 1)[0]
+os.chdir(cwd)
+
+p = PtyProcessUnicode.spawn(["deno", "run", "--unstable", file])
 
 scan = []
-scan.append("#" + sys.argv[1])
-while True:
-    try:
-        line = p.read()
-    except EOFError:
-        break
+scan.append("#" + file)
+try:
+    while True:
+        try:
+            line = p.read()
+        except EOFError:
+            break
 
-    print(line)
+        print(line)
 
-    if '⚠️ ' in line:
-        permission_type = line.split(' ')[4]
-        permission = line.split(' ')[7]
-        scan.append(permission_type + ' ' + permission)
-        p.write('y\n')
+        if '⚠️ ' in line:
+            permission_type = line.split(' ')[4]
+            permission = line.split(' ')[7]
+            scan.append(permission_type + ' ' + permission)
+            p.write('y\n')
+except KeyboardInterrupt:
+    pass
 
 # write scan.out
 with open('scan.out', 'w') as f:
